28885fab25504ca526913eb1c49c59a34ea5f83b
Jeremiah Akpera <jerryakpera@gmail.com>
2022-11-30 14:02:29 +0100

    Latest

--- config.js
+++ config.js
@@ -2,6 +2,24 @@ require('dotenv').config();
 
 const { env } = process;
 
+const dev_firebase_config = {
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_DOMAIN,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER_ID,
+  appId: env.APP_ID,
+};
+
+const prod_firebase_config = {
+  apiKey: env.PROD_API_KEY,
+  authDomain: env.PROD_AUTH_DOMAIN,
+  projectId: env.PROD_PROJECT_ID,
+  storageBucket: env.PROD_STORAGE_BUCKET,
+  messagingSenderId: env.PROD_MESSAGING_SENDER_ID,
+  appId: env.PROD_APP_ID,
+};
+
 module.exports = {
   emailAuth: {
     user: env.EMAIL_USER,
@@ -12,23 +30,8 @@ module.exports = {
 
   tokenSecret: env.TOKEN_SECRET,
 
-  firebaseConfig: {
-    apiKey: env.API_KEY,
-    authDomain: env.AUTH_DOMAIN,
-    projectId: env.PROJECT_ID,
-    storageBucket: env.STORAGE_BUCKET,
-    messagingSenderId: env.MESSAGING_SENDER_ID,
-    appId: env.APP_ID,
-  },
-
-  prodFirebaseConfig: {
-    apiKey: env.PROD_API_KEY,
-    authDomain: env.PROD_AUTH_DOMAIN,
-    projectId: env.PROD_PROJECT_ID,
-    storageBucket: env.PROD_STORAGE_BUCKET,
-    messagingSenderId: env.PROD_MESSAGING_SENDER_ID,
-    appId: env.PROD_APP_ID,
-  },
+  firebaseConfig:
+    env.NODE_ENV === 'production' ? prod_firebase_config : dev_firebase_config,
 
   baseURL: env.NODE_ENV === 'production' ? env.PROD_BASE_URL : env.DEV_BASE_URL,
 };
