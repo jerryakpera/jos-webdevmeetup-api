cc84e4179cf6ce645717234cf7643d0fd2c509f5
Jeremiah Akpera <jerryakpera@gmail.com>
2022-11-30 14:43:50 +0100

    Latest

--- utils/get-welcome-email-body.js
+++ utils/get-welcome-email-body.js
@@ -14,7 +14,7 @@ module.exports = (name) => {
       
       <h4 style="margin-bottom: 0; padding-bottom: 0"> Why? </h4>
       <p style="margin-top: 5px">
-      There is potential for a strong developer community to exist and thrive in Jos. Knowing ourselves will allow better and easier sharing of ideas, resources, opportunities, and solutions.Whether you're looking for to learn something new or find a friend, mentor, or partner this is the event to facilitate that union.
+      There is potential for a strong developer community to exist and thrive in Jos. Knowing ourselves will allow better and easier sharing of ideas, resources, opportunities, and solutions.Whether you're looking to acquire a friend, mentor, partner or new knowledge this is the event to facilitate that union.
       </p>
 
       <h4 style="margin-bottom: 0; padding-bottom: 0"> What? </h4>
@@ -22,7 +22,7 @@ module.exports = (name) => {
       The Jos WebDev Con simply put, is roughly 3 hours on the 10th of December, 2022 where beginners, developers, architects, project managers, and web enthusiasts can meet, network and learn in a relaxed environment. It is an opportunity to share problems your facing at work with real life people. Talk about projects that your working on or ideas that you are developing. 
 
       <br />
-      You will have the chance to discuss new technologies, design patters and solutions in the industry and how you can apply them in your work. It is not a developer boot camp but we will try to cater to all levels of developers. We would make sure to cover topics that range through different expertise, tech, and experience levels.
+      You will have the chance to discuss new technologies, design patterns and solutions in the industry and how you can apply them in your work. It is not a developer boot camp but we will try to cater to all levels of developers. We would make sure to cover topics that range through different expertise, tech, and experience levels.
       </p>
 
       <h4 style="margin-bottom: 0; padding-bottom: 0">Logistics</h4>
