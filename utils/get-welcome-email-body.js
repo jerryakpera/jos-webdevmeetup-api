870e34440a363ae5b2b4c834e31a9a5cee8ee262
Jeremiah Akpera <jerryakpera@gmail.com>
2022-11-30 13:44:00 +0100

    Latest

--- utils/get-welcome-email-body.js
+++ utils/get-welcome-email-body.js
@@ -4,7 +4,7 @@ module.exports = (name) => {
       <p style="margin-top: 5px"> Hello, ${name} </p>
       
       <p style="margin-top: 5px">
-        Firstly, my name is Jeremiah Akpera and I say thank you for officially becoming a pioneer member of the Jos WebDev Meetup. While still recognizing that this is the debut event I must also get ahead of myself and say I believe that many more will follow.
+        Firstly, my name is Jeremiah Akpera and I say thank you for officially becoming a pioneer member of the Jos WebDev Meetup. I hope that many more will follow.
       </p>
       
       <p style="margin-top: 5px">
@@ -14,20 +14,20 @@ module.exports = (name) => {
       
       <h4 style="margin-bottom: 0; padding-bottom: 0"> Why? </h4>
       <p style="margin-top: 5px">
-      Over my years in Jos, I have bumped into & rubbed minds with less than 5 developers in total. What usually follows is a bout of questions and answers. We try to share ideas, problems, and solutions but just in passing because we all have somewhere to be. Although few and far between, these meetings always have a way of pointing or refocusing me in the right direction.
-      There is a community-sized hole and we need to fill it. Whether you're looking for a friend, mentor, or partner this is an event to facilitate that union.
+      There is potential for a strong developer community to exist and thrive in Jos. Knowing ourselves will allow better and easier sharing of ideas, resources, opportunities, and solutions.Whether you're looking for to learn something new or find a friend, mentor, or partner this is the event to facilitate that union.
       </p>
 
       <h4 style="margin-bottom: 0; padding-bottom: 0"> What? </h4>
       <p style="margin-top: 5px">
-      The Jos WebDev Con simply put, is 3 hours on the 10th of December, 2022 where beginners, developers, architects, project managers, and web enthusiasts can meet, play and learn in a relaxed environment.
-      If I were going to one I would want it to be three things; welcoming, fun, and educative. Welcoming means I will be heard and respected. Fun means interesting and lively and educative means learning new things from those who know things I do not.
-      It is not a developer boot camp but we will try to cater to all levels of developers. We would make sure to cover topics that range through different expertise, tech, and experience levels.
+      The Jos WebDev Con simply put, is roughly 3 hours on the 10th of December, 2022 where beginners, developers, architects, project managers, and web enthusiasts can meet, network and learn in a relaxed environment. It is an opportunity to share problems your facing at work with real life people. Talk about projects that your working on or ideas that you are developing. 
+
+      <br />
+      You will have the chance to discuss new technologies, design patters and solutions in the industry and how you can apply them in your work. It is not a developer boot camp but we will try to cater to all levels of developers. We would make sure to cover topics that range through different expertise, tech, and experience levels.
       </p>
 
       <h4 style="margin-bottom: 0; padding-bottom: 0">Logistics</h4>
       <p style="margin-top: 5px">
-      If you can offer any sort of service or help to make this meetup run more smoothly I would greatly appreciate it. Here is a list of all the logistics that must be catered for that day;
+      Here is a list of all the logistics that must be catered for that day. If you are inclined to provide any of the items listed below please reach out to me;
 
         <ol>
           <li>
@@ -58,7 +58,7 @@ module.exports = (name) => {
       </p>
 
       <p style="margin-top: 5px">
-      If you are driven to assist in any way please reach out so we can talk. Assistance does not have to be monetary I would prefer the offering of skills, volunteering to bring a camera, projector, coffee, flash drives, etc, but I wouldn't say no to contributions. Few must sacrifice for many to benefit and I am looking to form a team to plan future meetups. You don't require any particular skills just the ability to work well in a team.
+      Assistance does not have to be monetary I would prefer skills, or volunteering to bring a camera, projector, coffee, flash drives, etc.
       </p>
 
       <p style="margin-top: 5px">
